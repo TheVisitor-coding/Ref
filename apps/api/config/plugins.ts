7d7c58e4b22bc18389113ce3b17a5185c5acbe98
Matteo ROSSI <matteorossiroy.pro@gmail.com>
2026-01-07 13:38:23 +0100

    add: smtp provider

--- apps/api/config/plugins.ts
+++ apps/api/config/plugins.ts
@@ -1,9 +1,23 @@
-export default () => ({
-    'users-permissions': {
-        config: {
-          jwt: {
-            expiresIn: '7d',
-          },
-        },
+export default ({ env }) => ({
+  'users-permissions': {
+    config: {
+      jwt: {
+        expiresIn: '7d',
       },
+    },
+  },
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST'),
+        port: env.int('SMTP_PORT'),
+        secure: env.bool('SMTP_SECURE', false), // true si 465, sinon false
+      },
+      settings: {
+        defaultFrom: env('SMTP_DEFAULT_FROM'),
+        defaultReplyTo: env('SMTP_DEFAULT_REPLY_TO'),
+      },
+    },
+  },
 });
